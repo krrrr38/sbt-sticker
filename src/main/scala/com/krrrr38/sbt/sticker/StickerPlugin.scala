d5e53ed1566693b837fd3bbb8e8d295f57f2aba6
krrrr38 <k.kaizu38@gmail.com>
2014-10-30 22:15:13 +0900

    fix plugin for alias command

--- src/main/scala/com/krrrr38/sbt/sticker/StickerPlugin.scala
+++ src/main/scala/com/krrrr38/sbt/sticker/StickerPlugin.scala
@@ -10,39 +10,68 @@ object StickerPlugin extends AutoPlugin {
 
   private[this] val stickerName = "sbt-sticker"
 
-  private[this] val stickerBriefHelp = ("sticker <command>", "sticker trigger")
   private[this] val stickerDetail = "Show notification as a sticker while a command running."
+  private[this] val stickerBriefHelp = ("sticker <command>", stickerDetail)
 
   lazy val sticker =
-    Command("sticker", stickerBriefHelp, stickerDetail)(BasicCommands.otherCommandParser) { (state, args) =>
-      var isSuccess = false
-      val startTime = System.currentTimeMillis()
-      val identifier = startTime + args
-      try {
-        Growl("at %tT" format new java.util.Date(startTime))
-          .name(stickerName)
-          .title(args)
-          .identifier(identifier)
-          .sticker(flag = true)
-          .image(StickerImage.getStartImagePath)
-          .exec
-        val result = Command.process(args, state)
-        result.onFailure.foreach(_ => isSuccess = true)
-        result
-      } finally {
-        val timeSec = (System.currentTimeMillis() - startTime) / 1000
-        val message =
-          if(isSuccess) {
-            s"[SUCCESS] Total time: $timeSec s"
-          } else {
-            s"[FAILURE] Total time: $timeSec s"
-          }
-        Growl(message)
-          .name(stickerName)
-          .title(args)
-          .identifier(identifier)
-          .image(StickerImage.getResultImagePath(isSuccess))
-          .exec
+    Command("sticker", stickerBriefHelp, stickerDetail)(BasicCommands.otherCommandParser) { (state, trigger) =>
+      def runNextCommand(state: State, isSuccess: Boolean): (State, Boolean) = {
+        if (state.hasNext) {
+          runNextCommand(state.runNext, isSuccess && state.hasSucceeded)
+        } else {
+          (state, isSuccess && state.hasSucceeded)
+        }
+      }
+
+      notify(trigger) {
+        val nextState = Command.process(trigger, state)
+        runNextCommand(nextState, isSuccess = true)
       }
     }
+
+  implicit class ContinuousState(state: State) {
+    def hasNext: Boolean = state.remainingCommands match {
+      // if command is executed on login shell, nothing remain.
+      // if command is executed on sbt shell, only "shell" command remain.
+      case Nil | "shell" :: _ => false
+      case _ => true
+    }
+
+    def runNext: State = {
+      val nextState = Command.process(state.remainingCommands.head, state)
+      nextState.copy(remainingCommands = state.remainingCommands.tail)
+    }
+
+    // same process in state.fail
+    def hasSucceeded: Boolean = state.remainingCommands.isEmpty || state.onFailure.isDefined
+  }
+
+  private[this] def notify(trigger: String)(commands: => (State, Boolean)): State = {
+    val startTime = System.currentTimeMillis()
+    val identifier = startTime + trigger
+    Growl("at %tT" format new java.util.Date(startTime))
+      .name(stickerName)
+      .title(trigger)
+      .identifier(identifier)
+      .sticker(flag = true)
+      .image(StickerImage.getStartImagePath)
+      .exec
+
+    val (state, isSuccess) = commands
+
+    val timeSec = (System.currentTimeMillis() - startTime) / 1000
+    val message =
+      if (isSuccess) {
+        s"[SUCCESS] Total time: $timeSec s"
+      } else {
+        s"[FAILURE] Total time: $timeSec s"
+      }
+    Growl(message)
+      .name(stickerName)
+      .title(trigger)
+      .identifier(identifier)
+      .image(StickerImage.getResultImagePath(isSuccess))
+      .exec
+    state
+  }
 }
